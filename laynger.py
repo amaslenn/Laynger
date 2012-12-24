f3182c9651509d2e1009040601c23a78ed3e9b7c
Andrey Maslennikov <andrew.maslennikov@gmail.com>
2012-12-24 05:09:12 -0800

    Create laynger.py

---
+++ laynger.py
@@ -0,0 +1,21 @@
+#import sublime
+import sublime_plugin
+
+
+class laynger(sublime_plugin.TextCommand):
+    def run(self, edit, opt='center'):
+        window = self.view.window()
+
+        layout = window.get_layout()
+
+        if len(layout['cols']) > 3:
+            return
+
+        if opt == u'center':
+            layout['cols'][1] = 0.5
+        elif opt == u'right':
+            layout['cols'][1] += 0.01
+        else:
+            layout['cols'][1] -= 0.01
+
+        window.run_command('set_layout', layout)
